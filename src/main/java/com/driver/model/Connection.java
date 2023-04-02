411b0cfdbf4d99d60accb0b65a739a6bd2694879
rjrahul08 <61372250+rjrahul08@users.noreply.github.com>
2023-04-03 01:50:29 +0530

    model updated

--- src/main/java/com/driver/model/Connection.java
+++ src/main/java/com/driver/model/Connection.java
@@ -2,31 +2,23 @@ package com.driver.model;
 
 import javax.persistence.*;
 
-// Note: Do not write @Enumerated annotation above CountryName in this model.
 @Entity
-public class Country {
+public class Connection {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private CountryName countryName;
-
-    private String code;
-
-    @OneToOne
+    @ManyToOne
     @JoinColumn
     User user;
 
-    @OneToMany
+
+    @ManyToOne
     @JoinColumn
     ServiceProvider serviceProvider;
 
-    public Country(){}
-
-    public Country(int id, CountryName countryName, String code, User user, ServiceProvider serviceProvider) {
+    public Connection(int id, User user, ServiceProvider serviceProvider) {
         this.id = id;
-        this.countryName = countryName;
-        this.code = code;
         this.user = user;
         this.serviceProvider = serviceProvider;
     }
@@ -37,22 +29,6 @@ public class Country {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public CountryName getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(CountryName countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public User getUser() {
---
+++ src/main/java/com/driver/model/ServiceProvider.java
@@ -0,0 +1,72 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class ServiceProvider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn
+    Admin admin;
+
+    @OneToMany(mappedBy = "serviceProvider",cascade = CascadeType.ALL)
+    List<Connection> connectionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "serviceProvider",cascade = CascadeType.ALL)
+    List<Country> countryList = new ArrayList<>();
+    public ServiceProvider(){}
+
+    public ServiceProvider(int id, String name, Admin admin, List<Connection> connectionList, List<Country> countryList) {
+        this.id = id;
+        this.name = name;
+        this.admin = admin;
+        this.connectionList = connectionList;
+        this.countryList = countryList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public List<Connection> getConnectionList() {
+        return connectionList;
+    }
+
+    public void setConnectionList(List<Connection> connectionList) {
+        this.connectionList = connectionList;
+    }
+
+    public List<Country> getCountryList() {
+        return countryList;
+    }
+
+    public void setCountryList(List<Country> countryList) {
+        this.countryList = countryList;
+    }
+}
